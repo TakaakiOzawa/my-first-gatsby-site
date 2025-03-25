b679864b59d79757c3b43ba8c0c36730866e68de
OZ01\takas <takasamapage@gmail.com>
2025-03-26 07:05:34 +0900

    デザイン変更

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,37 +4,10 @@ import '../styles/global.css'
 
 const Layout = ({ children }) => {
     return (
-        <div className="drawer drawer-end">
-            <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content">
-                <header>
-                    <div className="navbar bg-[#facc15]">
-                        <div className="flex-1">
-                            <StaticImage
-                                alt="Kagegonomi Logo 1"
-                                src="../images/Kagegonomi_logo.png"
-                                height={30}
-                            />
-                        </div>
-                        <div className="flex-none">
-                            <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary btn-square btn-ghost">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                            </label>
-                        </div>
-                    </div>
-                </header>
-                <main>
-                    {children}
-                </main>
-            </div>
-            <div className="drawer-side">
-                <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-                    <li><a>TOP</a></li>
-                    <li><a>ABOUT</a></li>
-                    <li><a>LATEST CHAPTER</a></li>
-                </ul>
-            </div>
+        <div>
+            <main>
+                {children}
+            </main>
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,126 +8,66 @@ import '../styles/global.css'
 const IndexPage = ( { data } ) => {
     return (
         <Layout pageTitle="Index">
-            <div className="relative">
+            {/*MAIN VISUAL*/}
+            <section className="relative">
                 <StaticImage
-                    alt="Kagegonomi TOP image"
-                    src="../images/Kagegonomi_top.jpg"
-                    className="w-full"
+                    src="../images/kagegonomi_mainvisual_temp.png"
+                    alt="Kagegonomi main visual"
+                    layout="fullWidth"
+                    placeholder="blurred"
+                    className="w-full h-auto"
                 />
-                <div className="absolute top-[7.5%] px-4">
+                <div className="absolute inset-0 flex flex-col items-center justify-center">
+                    {/* タイトル画像など */}
+                </div>
+            </section>
+            {/*WHAT'S NEW*/}
+            <section className="relative pt-[100px] pb-[80px]" id="whats_new">
+                <h2 className="text-center mb-[82px]">
                     <StaticImage
-                        alt="Kagegonomi string image"
-                        src="../images/Kagegonomi_logo_2.png"
-                        className="w-full max-w-screen-sm mx-auto"
+                        src="../images/head_whatsnew.png"
+                        alt="WHAT'S NEW | 新着情報"
                     />
-                </div>
-                <div className="absolute top-[32%] left-4 px-4">
-                    <StaticImage
-                        alt="Kagegonomi subtitle string image"
-                        src="../images/subtitle_5d.png"
-                        className="w-full max-w-screen-sm mx-auto"
-                    />
-                </div>
-                <div className="absolute bottom-28 px-4">
-                    <StaticImage
-                        alt="IkebukuroFM string image"
-                        src="../images/ikebukurofm_5d.png"
-                        className="w-full max-w-screen-sm mx-auto"
-                    />
-                </div>
-                <div className="absolute left-8 bottom-20 px-4">
-                    <StaticImage
-                        alt="Housou youbi jikan string image"
-                        src="../images/jikan_5d.png"
-                        className="w-full max-w-screen-sm mx-auto"
-                    />
-                </div>
-                <div className="absolute bottom-2 right-4 flex gap-4 p-2">
-                    <a href="https://x.com/kagegonomi" target="_blank" rel="noopener noreferrer">
-                        <StaticImage
-                            alt="X logo"
-                            src="../images/x_logo.png"
-                            className="w-16 h-16"
-                        />
-                    </a>
-                    <a href="https://note.com/kagegonomi/" target="_blank" rel="noopener noreferrer">
-                        <StaticImage
-                            alt="X logo"
-                            src="../images/note_logo.png"
-                            className="w-16 h-16"
-                        />
-                    </a>
-                </div>
-            </div>
-            <div className="grid grid-cols-2">
-                <a href="http://ikebukurofm.com/concept.html" target="_blank" rel="noopener noreferrer" className="btn btn-outline btn-info rounded-none no-animation w-full">池袋FM視聴リンク 📻</a>
-                <a href="https://t.co/P2kxaIgMmJ" target="_blank" rel="noopener noreferrer" className="btn btn-outline btn-error rounded-none no-animation w-full">番組への投稿フォーム 📧</a>
-            </div>
-            <div className="relative">
-                <StaticImage
-                    alt="Kagegonomi ABOUT image"
-                    src="../images/Kagegonomi_about_up_half.jpg"
-                    className="w-full"
-                />
-                <div className="absolute w-36 top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                    <StaticImage
-                        alt="Kagegonomi ABOUT string"
-                        src="../images/Kagegonomi_about_string.png"
-                        className="w-full"
-                    />
-                </div>
-                <div className="absolute mt-6 inset-0 flex items-center justify-center text-white">
-                    人気パーソナリティー「DJ浅い人間」が、<br/>
-                    毎週土曜深夜24時から池袋FMより<br/>
-                    根暗な独身の鬱憤を代弁する激動の30分！<br/>
-                </div>
-            </div>
-            <div className="relative">
-                <StaticImage
-                    alt="Kagegonomi PROFILE image"
-                    src="../images/okumono_tulip15.png"
-                    className="w-full"
-                />
-                <div className="absolute w-60 top-[20%] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                    <StaticImage
-                        alt="Kagegonomi PERSONALITY string"
-                        src="../images/Kagegonomi_personality_string.png"
-                        className="w-full"
-                    />
-                </div>
-                <div className="absolute avatar top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                    <div className="w-24 rounded-full">
-                        <StaticImage
-                            alt="Kagegonomi DJ AsaiNingen image"
-                            src="../images/Kagegonomi_DJ_AsaiNingen_face.jpg"
-                        />
+                </h2>
+                <div className="w-[1080px] mx-auto">
+                    <div className="inset-0 flex items-center justify-center mb-10">
+                        <div className="w-full mx-auto">
+                            {data.allMicrocmsContents.edges.map((node) => (
+                                <Link
+                                    to={`/contents/${node.node.id}`}
+                                    key={node.node.id}
+                                    className="block p-4 border-b border-gray-300 hover:bg-gray-100 transition duration-300"
+                                >
+                                    <article key={node.node.id}>
+                                        <p className="text-blue-600 text-sm font-bold mb-2">{node.node.createdAt}</p>
+                                        <p className="text-base font-bold">{node.node.title}</p>
+                                    </article>
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="text-right">
+                        <Link
+                            to={`/contents`}
+                            className="bottom-[2.5%] right-6 w-36 btn btn-outline"
+                        >
+                            MORE
+                        </Link>
                     </div>
                 </div>
-                <div className="absolute w-60 top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            </section>
+            {/*LATEST CHAPTER*/}
+            <section className="relative pt-[100px] pb-[80px]" id="latest_chapter">
+                <h2 className="text-center mb-[82px]">
                     <StaticImage
-                        alt="Kagegonomi DJ AsaiNingen string"
-                        src="../images/Kagegonomi_DJ_AsaiNingen.png"
-                        className="w-full"
+                        src="../images/head_latestchapter.png"
+                        alt="LATEST CHAPTER | 最新回"
                     />
-                </div>
-            </div>
-            <div className="relative">
-                <StaticImage
-                    alt="Kagegonomi Latest Chapter back image"
-                    src="../images/Kagegonomi_Latest_Chapter_back.png"
-                    className="w-full"
-                />
-                <div className="absolute w-80 top-[20%] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                    <StaticImage
-                        alt="Kagegonomi LATEST CHAPTER string"
-                        src="../images/Kagegonomi_latest_chapter_string.png"
-                        className="w-full"
-                    />
-                </div>
-                <div className="absolute mt-8 w-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  px-4">
+                </h2>
+                <div className="w-[1080px] mx-auto">
                     <iframe
                         src="https://www.youtube.com/embed/?list=UUQ11o0iwgUQ84MHMQtftoJQ"
-                        title="Latest Video"
+                        title="latest chapter"
                         width="100%"
                         height="152"
                         allowFullScreen
@@ -135,53 +75,14 @@ const IndexPage = ( { data } ) => {
                         loading="lazy"
                     ></iframe>
                 </div>
-            </div>
-            <div className="relative">
-                <StaticImage
-                    alt="Kagegonomi back image"
-                    src="../images/white_back.jpg"
-                    className="w-full"
-                />
-                <div className="absolute w-60 top-[12.5%] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                    <StaticImage
-                        alt="Kagegonomi WHAT'S NEW string"
-                        src="../images/Kagegonomi_whatsnew_string.png"
-                        className="w-full"
-                    />
-                </div>
-                <div className="absolute inset-0 flex items-center justify-center">
-                    <div className="max-w-4xl w-full mx-auto px-4">
-                        {data.allMicrocmsContents.edges.map((node) => (
-                            <Link
-                                to={`/contents/${node.node.id}`}
-                                key={node.node.id}
-                                className="block p-4 border-b border-gray-300 hover:bg-gray-100 transition duration-300"
-                            >
-                                <article key={node.node.id}>
-                                    <p className="text-blue-600 text-sm font-bold mb-2">{node.node.createdAt}</p>
-                                    <p className="text-base font-bold">{node.node.title}</p>
-                                </article>
-                            </Link>
-                        ))}
-                    </div>
-                    <Link
-                        to={`/contents`}
-                        className="absolute bottom-[2.5%] right-6 w-36 btn btn-outline"
-                    >
-                        MORE
-                    </Link>
-                </div>
-            </div>
-            <div className="text-sm bg-[#facc15] text-black p-4">
-                Copyright © 2025 Kagegonomi All Rights Reserved.
-            </div>
+            </section>
         </Layout>
     )
 }
 
 export const query = graphql`
     query {
-        allMicrocmsContents(sort: { createdAt: DESC }, limit: 3) {
+        allMicrocmsContents(sort: { createdAt: DESC }, limit: 4) {
             edges {
                 node {
                     id
