ba4e817e79f1bb9eb508ebde4d6c4858607485ec
OZ01\takas <takasamapage@gmail.com>
2025-04-07 20:41:43 +0900

    個別記事ページ修正

--- src/templates/content-detail.js
+++ src/templates/content-detail.js
@@ -8,14 +8,26 @@ const ContentDetail = ({ data }) => {
 
     return (
         <Layout pageTitle={title}>
-            <div className="container mx-auto px-4">
-                <h1 className="text-2xl font-bold">{title}</h1>
-                <p className="text-gray-600">{createdAt}</p>
-                {category && <p className="text-blue-500">{category.name}</p>}
-                <div
-                    className="prose prose-sm lg:prose-lg"
-                    dangerouslySetInnerHTML={{ __html: content }}
-                />
+            <div className="container mx-auto px-4 py-[70px]">
+                <section>
+                    <article className="min-h-screen py-12 px-6 text-[1.4rem]">
+                        <div className="mb-[6px]">
+                            <time className="text-blue-500 text-[0.9rem] font-bold antialiased">
+                                {createdAt}
+                            </time>
+                        </div>
+                        <div className="mb-16">
+                            <h3 className="text-[1.1rem] font-bold leading-[1.4]">
+                                {title}
+                            </h3>
+                        </div>
+                        {/*{category && <p className="text-blue-500">{category.name}</p>}*/}
+                        <div
+                            className="prose prose-sm lg:prose-lg"
+                            dangerouslySetInnerHTML={{ __html: content }}
+                        />
+                    </article>
+                </section>
             </div>
         </Layout>
     );
