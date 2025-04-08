651db67b9369ffc4bdad5675a7039475a2584e4a
OZ01\takas <takasamapage@gmail.com>
2025-04-09 03:05:30 +0900

    個別記事ページ調整

--- src/templates/content-detail.js
+++ src/templates/content-detail.js
@@ -10,17 +10,18 @@ const ContentDetail = ({ data }) => {
         <Layout pageTitle={title}>
             <div className="container mx-auto px-4 py-[70px]">
                 <section>
-                    <article className="min-h-screen py-12 px-6 text-[1.4rem]">
+                    <article className="min-h-screen py-12 px-12 text-[1.4rem]">
                         <div className="mb-[6px]">
                             <time className="text-blue-500 text-[0.9rem] font-bold antialiased">
                                 {createdAt}
                             </time>
                         </div>
-                        <div className="mb-16">
+                        <div className="mb-4">
                             <h3 className="text-[1.1rem] font-bold leading-[1.4]">
                                 {title}
                             </h3>
                         </div>
+                        <div className="mb-12 border-b border-gray-300" />
                         {/*{category && <p className="text-blue-500">{category.name}</p>}*/}
                         <div
                             className="prose prose-sm lg:prose-lg"
