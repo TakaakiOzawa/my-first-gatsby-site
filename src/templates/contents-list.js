9664b22b189134948a077f896511752a59fd6010
OZ01\takas <takasamapage@gmail.com>
2025-04-09 03:04:46 +0900

    記事一覧ページ一旦完成

--- src/templates/contents-list.js
+++ src/templates/contents-list.js
@@ -14,107 +14,99 @@ const ContentsList = ({ data, pageContext }) => {
 
     return (
         <Layout pageTitle="WHAT'S NEW">
-            <div
-                className="relative"
-                style={{
-                    height: `${data.allMdx.nodes.length * 80 + 200}px`,
-                }}
-            >
-                <StaticImage
-                    alt="Kagegonomi back image"
-                    src="../images/white_back_contents.jpg"
-                    className="w-full h-full"
-                />
-                <div className="absolute w-60 top-12 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                    <StaticImage
-                        alt="Kagegonomi WHAT'S NEW string"
-                        src="../images/Kagegonomi_whatsnew_string.png"
-                        className="w-full"
-                    />
-                </div>
-                <div className="absolute inset-0 flex items-center justify-center">
-                    <div className="max-w-4xl w-full mx-auto px-4">
-                        {data.allMdx.nodes.map((node) => (
-                            <Link
-                                to={`/contents/${node.frontmatter.slug}`}
-                                key={node.id}
-                                className="block p-4 border-b border-gray-300 hover:bg-gray-100 transition duration-300"
-                            >
-                                <article key={node.id}>
-                                    <p className="text-blue-600 text-sm font-bold mb-2">{node.frontmatter.date}</p>
-                                    <p className="text-base font-bold">{node.frontmatter.title}</p>
-                                </article>
-                            </Link>
-                        ))}
-                    </div>
-                </div>
-                <div className="absolute bottom-4 w-full flex justify-center mt-2 space-x-2">
-                    {!isFirst && (
-                        <Link
-                            to={prevPage}
-                            className="px-4 py-2 border border-gray-300 rounded-lg text-gray-600 hover:bg-gray-100"
-                        >
-                            ←
-                        </Link>
-                    )}
-                    {Array.from({ length: numPages }, (_, i) => {
-                        const pageNumber = i + 1;
-
-                        const shouldShow =
-                        pageNumber === 1 ||
-                        pageNumber === numPages ||
-                        (pageNumber >= currentPage - 2 && pageNumber <= currentPage + 2);
-
-                        const isEllipsisBefore =
-                        pageNumber === currentPage - 3 && pageNumber !== 2;
-                        const isEllipsisAfter =
-                        pageNumber === currentPage + 3 && pageNumber !== numPages - 1;
-
-                        return (
-                            <React.Fragment key={pageNumber}>
-                                {isEllipsisBefore && <span className="px-2">...</span>}
-                                {shouldShow && (
+            <div className="py-18">
+                <section>
+                    <h2 className="text-center mt-28">
+                        <StaticImage
+                            src="../images/head_whatsnew.png"
+                            alt="WHAT'S NEW | 新着情報"
+                            height={60}
+                            layout="fixed"
+                            className="block mx-auto"
+                        />
+                    </h2>
+                    <div className="min-h-screen py-12 text-[1.4rem]">
+                        <div className="w-[1080px] mx-auto">
+                            {data.allMicrocmsContents.edges.map((node) => (
+                                <Link
+                                    to={`/contents/${node.node.id}`}
+                                    key={node.node.id}
+                                    className="block p-4 border-b border-gray-300 hover:bg-gray-100 transition duration-300"
+                                >
+                                    <article key={node.node.id}>
+                                        <p className="text-blue-600 text-sm font-bold mb-2">{node.node.createdAt}</p>
+                                        <p className="text-base font-bold">{node.node.title}</p>
+                                    </article>
+                                </Link>
+                            ))}
+                            <div className="bottom-4 w-full flex justify-center mt-4 space-x-2">
+                                {!isFirst && (
                                     <Link
-                                        to={pageNumber === 1 ? "/contents" : `/contents/${pageNumber}`}
-                                        className={`px-4 py-2 border rounded-lg ${
-                                        currentPage === pageNumber
-                                            ? "bg-blue-500 text-white border-blue-500"
-                                            : "border-gray-300 text-gray-600 hover:bg-gray-100"
-                                        }`}
+                                        to={prevPage}
+                                        className="px-4 py-2 rounded-lg text-gray-600 hover:bg-gray-100"
                                     >
-                                        {pageNumber}
+                                        ←
                                     </Link>
                                 )}
-                                {isEllipsisAfter && <span className="px-2">...</span>}
-                            </React.Fragment>
-                        );
-                    })}
-                    {!isLast && (
-                        <Link
-                            to={nextPage}
-                            className="px-4 py-2 border border-gray-300 rounded-lg text-gray-600 hover:bg-gray-100"
-                        >
-                            →
-                        </Link>
-                    )}
-                </div>
-            </div>
-            <div className="text-sm bg-[#facc15] text-black p-4">
-                Copyright © 2025 Kagegonomi All Rights Reserved.
+                                {Array.from({ length: numPages }, (_, i) => {
+                                    const pageNumber = i + 1;
+
+                                    const shouldShow =
+                                        pageNumber === 1 ||
+                                        pageNumber === numPages ||
+                                        (pageNumber >= currentPage - 2 && pageNumber <= currentPage + 2);
+
+                                    const isEllipsisBefore =
+                                        pageNumber === currentPage - 3 && pageNumber !== 2;
+                                    const isEllipsisAfter =
+                                        pageNumber === currentPage + 3 && pageNumber !== numPages - 1;
+
+                                    return (
+                                        <React.Fragment key={pageNumber}>
+                                            {isEllipsisBefore && <span className="px-2">...</span>}
+                                            {shouldShow && (
+                                                <Link
+                                                    to={pageNumber === 1 ? "/contents" : `/contents/${pageNumber}`}
+                                                    className={`px-4 py-2 rounded-lg ${currentPage === pageNumber
+                                                        ? "bg-blue-500 text-white"
+                                                        : "text-gray-600 hover:bg-gray-100"
+                                                        }`}
+                                                >
+                                                    {pageNumber}
+                                                </Link>
+                                            )}
+                                            {isEllipsisAfter && <span className="px-2">...</span>}
+                                        </React.Fragment>
+                                    );
+                                })}
+                                {!isLast && (
+                                    <Link
+                                        to={nextPage}
+                                        className="px-4 py-2 rounded-lg text-gray-600 hover:bg-gray-100"
+                                    >
+                                        →
+                                    </Link>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         </Layout>
     );
 };
 
 export const query = graphql`
-    query BlogListQuery($skip: Int!, $limit: Int!) {
-        allMdx(sort: { frontmatter: { date: DESC } }, skip: $skip, limit: $limit) {
-            nodes {
-                id
-                frontmatter {
+    query($skip: Int!, $limit: Int!) {
+        allMicrocmsContents(sort: { createdAt: DESC }, skip: $skip, limit: $limit) {
+            edges {
+                node {
+                    id
                     title
-                    date(formatString: "YYYY.MM.DD")
-                    slug
+                    createdAt(formatString: "YYYY.MM.DD")
+                    category {
+                        name
+                    }
                 }
             }
         }
