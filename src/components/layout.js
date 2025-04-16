0c3732313a97d0d65914c0931647f75ee5fe2691
OZ01\takas <takasamapage@gmail.com>
2025-04-16 23:53:09 +0900

    Layoutレスポンシブ対応

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
         <div className="drawer drawer-end">
             <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
-                <div className="navbar bg-yellow-500 shadow-sm fixed top-0 left-0 right-0 z-50">
+                <div className="navbar bg-yellow-500 shadow-sm fixed top-0 left-0 right-0 z-50 max-md:min-h-12 max-md:h-12">
                     <div className="navbar-start">
                         <Link to="/" className="btn btn-ghost">
                             <StaticImage
@@ -17,11 +17,19 @@ const Layout = ({ children }) => {
                                 layout="fixed"
                                 height={40}
                                 loading="eager"
-                                className="block mx-auto"
+                                className="md:block hidden mx-auto"
+                            />
+                            <StaticImage
+                                src="../images/kagegonomi_logo.png"
+                                alt="Kagegonomi Logo"
+                                layout="fixed"
+                                height={26}
+                                loading="eager"
+                                className="md:hidden block mx-auto"
                             />
                         </Link>
                     </div>
-                    <div className="navbar-center hidden lg:flex">
+                    <div className="navbar-center hidden md:flex">
                         <ul className="menu menu-horizontal px-1">
                             <li><a href="/#whats_new">WHAT'S NEW</a></li>
                             <li><a href="/#latest_episode">LATEST EPISODE</a></li>
@@ -31,8 +39,8 @@ const Layout = ({ children }) => {
                         </ul>
                     </div>
                     <div className="navbar-end">
-                        <label htmlFor="my-drawer-4" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <label htmlFor="my-drawer-4" className="btn btn-ghost md:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
                             </svg>
                         </label>
@@ -41,7 +49,7 @@ const Layout = ({ children }) => {
                 <main>
                     {children}
                 </main>
-                <div className="text-sm bg-yellow-500 text-black p-4">
+                <div className="text-sm bg-yellow-500 text-black p-4 h-12">
                     Copyright © 2025 Kagegonomi All Rights Reserved.
                 </div>
             </div>
