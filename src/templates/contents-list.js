606bee85068fef9dff71ea0f96207e2a7a49b097
OZ01\takas <takasamapage@gmail.com>
2025-04-20 21:30:17 +0900

    記事一覧画面レスポンシブ対応

--- src/templates/contents-list.js
+++ src/templates/contents-list.js
@@ -14,9 +14,9 @@ const ContentsList = ({ data, pageContext }) => {
 
     return (
         <Layout pageTitle="WHAT'S NEW">
-            <div className="py-16">
+            <div className="container mx-auto px-4 pt-16 pb-8">
                 <section>
-                    <h2 className="text-center mt-20">
+                    <h2 className="text-center mt-20 max-md:mt-6 max-md:mb-4">
                         <StaticImage
                             src="../images/head_whatsnew.png"
                             alt="WHAT'S NEW | 新着情報"
@@ -25,69 +25,67 @@ const ContentsList = ({ data, pageContext }) => {
                             className="block mx-auto"
                         />
                     </h2>
-                    <div className="min-h-screen py-12 text-[1.4rem]">
-                        <div className="w-[1080px] mx-auto">
-                            {data.allMicrocmsContents.edges.map((node) => (
+                    <div className="min-h-screen px-12 py-12 max-md:px-2 max-md:py-0 text-xl max-md:text-base">
+                        {data.allMicrocmsContents.edges.map((node) => (
+                            <Link
+                                to={`/contents/${node.node.id}`}
+                                key={node.node.id}
+                                className="block p-4 border-b border-gray-300 hover:bg-gray-100 transition duration-300"
+                            >
+                                <article key={node.node.id}>
+                                    <p className="text-blue-600 text-sm font-bold mb-2">{node.node.createdAt}</p>
+                                    <p className="text-base max-md:text-sm font-bold">{node.node.title}</p>
+                                </article>
+                            </Link>
+                        ))}
+                        <div className="bottom-4 w-full flex justify-center mt-4 space-x-2">
+                            {!isFirst && (
                                 <Link
-                                    to={`/contents/${node.node.id}`}
-                                    key={node.node.id}
-                                    className="block p-4 border-b border-gray-300 hover:bg-gray-100 transition duration-300"
+                                    to={prevPage}
+                                    className="px-4 py-2 rounded-sm text-gray-600 hover:bg-gray-100"
                                 >
-                                    <article key={node.node.id}>
-                                        <p className="text-blue-600 text-sm font-bold mb-2">{node.node.createdAt}</p>
-                                        <p className="text-base font-bold">{node.node.title}</p>
-                                    </article>
+                                    ←
                                 </Link>
-                            ))}
-                            <div className="bottom-4 w-full flex justify-center mt-4 space-x-2">
-                                {!isFirst && (
-                                    <Link
-                                        to={prevPage}
-                                        className="px-4 py-2 rounded-lg text-gray-600 hover:bg-gray-100"
-                                    >
-                                        ←
-                                    </Link>
-                                )}
-                                {Array.from({ length: numPages }, (_, i) => {
-                                    const pageNumber = i + 1;
+                            )}
+                            {Array.from({ length: numPages }, (_, i) => {
+                                const pageNumber = i + 1;
 
-                                    const shouldShow =
-                                        pageNumber === 1 ||
-                                        pageNumber === numPages ||
-                                        (pageNumber >= currentPage - 2 && pageNumber <= currentPage + 2);
+                                const shouldShow =
+                                    pageNumber === 1 ||
+                                    pageNumber === numPages ||
+                                    (pageNumber >= currentPage - 2 && pageNumber <= currentPage + 2);
 
-                                    const isEllipsisBefore =
-                                        pageNumber === currentPage - 3 && pageNumber !== 2;
-                                    const isEllipsisAfter =
-                                        pageNumber === currentPage + 3 && pageNumber !== numPages - 1;
+                                const isEllipsisBefore =
+                                    pageNumber === currentPage - 3 && pageNumber !== 2;
+                                const isEllipsisAfter =
+                                    pageNumber === currentPage + 3 && pageNumber !== numPages - 1;
 
-                                    return (
-                                        <React.Fragment key={pageNumber}>
-                                            {isEllipsisBefore && <span className="px-2">...</span>}
-                                            {shouldShow && (
-                                                <Link
-                                                    to={pageNumber === 1 ? "/contents" : `/contents/${pageNumber}`}
-                                                    className={`px-4 py-2 rounded-lg ${currentPage === pageNumber
-                                                        ? "bg-blue-500 text-white"
-                                                        : "text-gray-600 hover:bg-gray-100"
-                                                        }`}
-                                                >
-                                                    {pageNumber}
-                                                </Link>
-                                            )}
-                                            {isEllipsisAfter && <span className="px-2">...</span>}
-                                        </React.Fragment>
-                                    );
-                                })}
-                                {!isLast && (
-                                    <Link
-                                        to={nextPage}
-                                        className="px-4 py-2 rounded-lg text-gray-600 hover:bg-gray-100"
-                                    >
-                                        →
-                                    </Link>
-                                )}
-                            </div>
+                                return (
+                                    <React.Fragment key={pageNumber}>
+                                        {isEllipsisBefore && <span className="px-2">...</span>}
+                                        {shouldShow && (
+                                            <Link
+                                                to={pageNumber === 1 ? "/contents" : `/contents/${pageNumber}`}
+                                                className={`px-4 py-2 rounded-sm ${currentPage === pageNumber
+                                                    ? "bg-blue-500 text-white"
+                                                    : "text-gray-600 hover:bg-gray-100"
+                                                    }`}
+                                            >
+                                                {pageNumber}
+                                            </Link>
+                                        )}
+                                        {isEllipsisAfter && <span className="px-2">...</span>}
+                                    </React.Fragment>
+                                );
+                            })}
+                            {!isLast && (
+                                <Link
+                                    to={nextPage}
+                                    className="px-4 py-2 rounded-sm text-gray-600 hover:bg-gray-100"
+                                >
+                                    →
+                                </Link>
+                            )}
                         </div>
                     </div>
                 </section>
